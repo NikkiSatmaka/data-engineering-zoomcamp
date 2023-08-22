5aa0da35b063461c3fa4b03e00856d76d2489440
Nikki Satmaka <nsatmaka@gmail.com>
2023-08-22 23:38:08 +0700

    ingest data using parquet

--- week_1_basics_n_setup/2_docker_sql/ingest_data.py
+++ week_1_basics_n_setup/2_docker_sql/ingest_data.py
@@ -1,78 +1,77 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-import os
 import argparse
-
-from time import time
+import time
 
 import pandas as pd
+import pyarrow.parquet as pq
+import requests
 from sqlalchemy import create_engine
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Ingest CSV data to Postgres")
+    parser.add_argument("--user", required=True, help="user name for postgres")
+    parser.add_argument("--password", required=True, help="password for postgres")
+    parser.add_argument("--host", required=True, help="host for postgres")
+    parser.add_argument("--port", required=True, help="port for postgres")
+    parser.add_argument("--db", required=True, help="database name for postgres")
+    parser.add_argument(
+        "--table_name",
+        required=True,
+        help="name of the table where we will write the results to",
+    )
+    parser.add_argument("--url", required=True, help="url of the parquet file")
+    args = parser.parse_args()
+    return args
 
 
 def main(params):
     user = params.user
     password = params.password
-    host = params.host 
-    port = params.port 
+    host = params.host
+    port = params.port
     db = params.db
     table_name = params.table_name
     url = params.url
-    
-    # the backup files are gzipped, and it's important to keep the correct extension
-    # for pandas to be able to open the file
-    if url.endswith('.csv.gz'):
-        csv_name = 'output.csv.gz'
-    else:
-        csv_name = 'output.csv'
+    csv_name = "output.parquet"
 
-    os.system(f"wget {url} -O {csv_name}")
+    with requests.Session() as s:
+        r = s.get(url)
 
-    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+    with open(csv_name, "wb") as f:
+        f.write(r.content)
 
-    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100000)
+    engine = create_engine(f"postgresql+psycopg://{user}:{password}@{host}:{port}/{db}")
 
-    df = next(df_iter)
+    parquet_file = pq.ParquetFile(csv_name)
+    parquet_iter = parquet_file.iter_batches(batch_size=100000)
 
-    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    df = next(parquet_iter).to_pandas()
 
-    df.head(n=0).to_sql(name=table_name, con=engine, if_exists='replace')
+    df.head(n=0).to_sql(name=table_name, con=engine, if_exists="replace")
 
-    df.to_sql(name=table_name, con=engine, if_exists='append')
+    df.to_sql(name=table_name, con=engine, if_exists="append")
 
-
-    while True: 
-
+    while True:
         try:
-            t_start = time()
-            
-            df = next(df_iter)
+            t_start = time.perf_counter()
 
-            df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-            df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+            df = next(parquet_iter).to_pandas()
 
-            df.to_sql(name=table_name, con=engine, if_exists='append')
+            df.to_sql(name=table_name, con=engine, if_exists="append")
 
-            t_end = time()
+            t_end = time.perf_counter()
 
-            print('inserted another chunk, took %.3f second' % (t_end - t_start))
+            print(f"inserted another chunk, took {t_end - t_start:.3f} second(s)")
 
         except StopIteration:
             print("Finished ingesting data into the postgres database")
             break
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
 
-    parser.add_argument('--user', required=True, help='user name for postgres')
-    parser.add_argument('--password', required=True, help='password for postgres')
-    parser.add_argument('--host', required=True, help='host for postgres')
-    parser.add_argument('--port', required=True, help='port for postgres')
-    parser.add_argument('--db', required=True, help='database name for postgres')
-    parser.add_argument('--table_name', required=True, help='name of the table where we will write the results to')
-    parser.add_argument('--url', required=True, help='url of the csv file')
-
-    args = parser.parse_args()
+if __name__ == "__main__":
+    args = parse_args()
 
     main(args)
